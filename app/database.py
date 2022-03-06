5ed28d29b9fb2f0a0f33778d019d54912e4c6aca
daitasu <ifctp.dai.monara@gmail.com>
2022-03-06 15:19:23 +0900

    Add basic crud api

---
+++ app/database.py
@@ -0,0 +1,13 @@
+﻿from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# SQLALCHEMY_DATABASE_URL = "postgresql://root:password@postgresserver/db"
+SQLALCHEMY_DATABASE_URL = (
+    "postgresql://localhost:15432/devdb?user=root&password=password"
+)
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()

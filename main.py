b96c0486611f9f6b3e31f4d32cd1dd28a8638300
daitasu <ifctp.dai.monara@gmail.com>
2022-03-26 23:21:05 +0900

    Refactor: move settings to config directory with starlette.config

--- main.py
+++ main.py
@@ -8,22 +8,17 @@ from jose import JWTError, jwt
 
 import app.models as models, app.schemas as schemas, app.crud as crud
 from app.database import SessionLocal, engine
+from app.core.config import SECRET_KEY, JWT_ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
 from app import auth_service
 
 models.Base.metadata.create_all(bind=engine)
-
-
-SECRET_KEY = "7361ca3778e32d70731a32868959e1c05a72042a1ba96b97c1294c6a9852d889"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
 
 fake_users_db = {
     "johndoe": {
         "username": "johndoe",
         "full_name": "John Doe",
         "email": "johndoe@example.com",
-        "hashed_password": "$2b$12$2Q8ZFyOZFO8YvqNvxb/aS.WHlXtLCH7Xax3X/M8z88QbnbYLdbKia",
+        "hashed_password": "$2b$12$ksGbpKo4SEVeYQImKWUYKubqqL/9B28E.ldD/u8Ed770w2rEquXy2",
         "disabled": False,
     }
 }
@@ -64,7 +59,7 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
     else:
         expire = datetime.utcnow() + timedelta(minutes=15)
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    encoded_jwt = jwt.encode(to_encode, str(SECRET_KEY), algorithm=JWT_ALGORITHM)
     return encoded_jwt
 
 
@@ -76,7 +71,7 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
     )
     try:
         # 受け取ったアクセストークンをdecodeする。payloadを分解して、usernameを取得
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        payload = jwt.decode(token, str(SECRET_KEY), algorithms=[JWT_ALGORITHM])
         username: str = payload.get("sub")
         if username is None:
             raise credentials_exception
@@ -136,38 +131,3 @@ async def read_own_items(
     current_user: schemas.AuthUser = Depends(get_current_active_user),
 ):
     return [{"item_id": "Foo", "owner": current_user.username}]
-
-
-# @app.post("/users/", response_model=schemas.User)
-# def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-#     db_user = crud.get_user_by_email(db, email=user.email)
-#     if db_user:
-#         raise HTTPException(status_code=400, detail="Email already registered")
-#     return crud.create_user(db=db, user=user)
-
-
-# @app.get("/users/", response_model=list[schemas.User])
-# def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-#     users = crud.get_users(db, skip=skip, limit=limit)
-#     return users
-
-
-# @app.get("/users/{user_id}", response_model=schemas.User)
-# def read_user(user_id: int, db: Session = Depends(get_db)):
-#     db_user = crud.get_user(db, user_id=user_id)
-#     if db_user is None:
-#         raise HTTPException(status_code=404, detail="User not found")
-#     return db_user
-
-
-# @app.post("/users/{user_id}/items/", response_model=schemas.Item)
-# def create_item_for_user(
-#     user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
-# ):
-#     return crud.create_user_item(db=db, item=item, user_id=user_id)
-
-
-# @app.get("/items/", response_model=list[schemas.Item])
-# def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-#     items = crud.get_items(db, skip=skip, limit=limit)
-#     return items
